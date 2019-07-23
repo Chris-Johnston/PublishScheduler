0b2a0533105c579e623798d7de1a27c8a2831488
Chris Johnston <Christopher.Johnston@microsoft.com>
2019-07-22 20:06:15 -0700

    add support for switching on the event type

--- GitHubWebhook.cs
+++ GitHubWebhook.cs
@@ -12,6 +12,14 @@ namespace PublishScheduler
 {
     public static class GitHubWebhook
     {
+        // the name of the header which indicates the type of event
+        private const string EventType = "X-GitHub-Event";
+        
+        private const string PullRequestEvent = "pull_request";
+        private const string IssueComentEvent = "issue_comment";
+
+
+
         [FunctionName("GitHubWebhook")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -19,9 +27,30 @@ namespace PublishScheduler
         {
             log.LogInformation("Got GitHub Webhook.");
 
+            string eventType = string.Empty;
+
+            if (req.Headers.ContainsKey(EventType))
+            {
+                eventType = req.Headers[EventType];
+                log.LogInformation($"Rec. event of type: {eventType}");
+            }
+
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
             log.LogInformation($"Request body: {requestBody}");
+
+
+            switch (eventType)
+            {
+                case PullRequestEvent:
+                    // do a thing
+                break;
+                case IssueComentEvent: // same event as a PR comment, need to check that this comment is made on a PR and not an issue
+                    // do a thing, but differently
+                break;
+            }
+
+
 
             return new OkObjectResult("OK");
         }
