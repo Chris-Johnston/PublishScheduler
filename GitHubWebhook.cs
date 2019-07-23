5e0c3b949a78db456ca80ffd3f8ab927246ad8e7
Chris Johnston <Christopher.Johnston@microsoft.com>
2019-07-22 18:22:43 -0700

    add github webhook that logs requests

---
+++ GitHubWebhook.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace PublishScheduler
+{
+    public static class GitHubWebhook
+    {
+        [FunctionName("GitHubWebhook")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            log.LogDebug($"Request body: {requestBody}");
+
+            return new OkObjectResult("OK");
+        }
+    }
+}
